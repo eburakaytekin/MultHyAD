2d964a2b4a4816d28f01f19bbeaf7b204ceaf061
eburakaytekin <aytekinburak@hotmail.com>
2021-10-28 20:58:52 +0300

    Delete GRX directory

--- GRX/AllGRX.m
+++
@@ -1,7 +0,0 @@
-function [AUCMeanResult,elapsedTimeResult,FARMeanResult,dtmapMeanArray]=AllGRX(data)
-
-tic
-[AUCMeanResult,FARMeanResult,dtmapMeanArray]=GRXfunc(data,map,DAT);
-elapsedTimeResult = toc;
-
-end
